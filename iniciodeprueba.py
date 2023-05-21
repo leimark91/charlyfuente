21561918bfa06f26d1233dce9d33744a45c07a60
charly2024 <luisnegoc91@gmail.com>
2023-05-20 23:24:45 -0300

    inicio modificado

---
+++ iniciodeprueba.py
@@ -0,0 +1,25 @@
+# mi calculadora de porcentaje de ganancia
+#Super edita funcion 2
+
+
+#super edisiones
+
+
+#cambios de todo en los programados.
+
+print ("ingrese el porciento que gana En PRESTAMOS: ")
+
+porcentaje=float (input())
+
+print ("ingrese cuanto PRESTO: ")
+
+numero=float(input())
+
+resultado =  porcentaje* numero/ 100 
+
+
+print(f"El % es igual : ", { resultado })
+
+SUMA= resultado + numero
+
+print(f"EL RESULTADO TOTAL ES =", {SUMA })
